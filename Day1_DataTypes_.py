07cbaf806ff5cff1b28d5137e2279a72f4604f4c
Gia Bảo <hoanggiabao131012vn@gmail.com>
2026-02-19 08:36:55 +0700

    Create Day1_DataTypes_.py

---
+++ Day1_DataTypes_.py
@@ -0,0 +1,11 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+int_variable = int(input())
+float_variable = float(input())
+str_variable = input()
+
+print(i + int_variable)
+print(d + float_variable)
+print(s + str_variable)
